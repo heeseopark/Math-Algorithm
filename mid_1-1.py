8c0ab77c381ace9a87b241f1696d407abc07d724
heeseopark <heeseopark@korea.ac.kr>
2022-10-26 23:23:43 +0900

    is_prime edited

--- mid_1-1.py
+++ mid_1-1.py
@@ -1,15 +1,25 @@
-def is_prime(n):
+def is_prime(n,print_solution):
+    if print_solution == 1:
+        if n<=0:
+            return print('%d is not a natural number.' %n)
 
-    if n<=0:
-        return print('%d is not a natural number.' %n)
-
-    if n == 1:
-        return print('%d is not a prime number nor a synthetic number' %n)
+        if n == 1:
+            return print('%d is not a prime number nor a synthetic number' %n)
 
 
-    for i in range(2, n):
-        if n%i == 0:
-            return print('%d is a synthetic number' %n)
-    
-    else:
-        return print('%d is a prime nubmer' %n)
+        for i in range(2, n):
+            if n%i == 0:
+                print('%d can be devided by %d' %(n,i))
+                return print('%d is a synthetic number' %n)
+        
+        else:
+            return print('%d is a prime nubmer' %n)
+
+    if print_solution == 0:
+        if i<2:
+            raise ValueError
+        for i in range(2, n):
+            if n % i == 0:
+                return False
+        return True
+
