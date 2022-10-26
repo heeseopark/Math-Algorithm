8eaf31c83e259bf6bd08279349707a9f4e939215
heeseopark <heeseopark@korea.ac.kr>
2022-10-26 23:14:13 +0900

    edit is_prime function

--- mid_1-1.py
+++ mid_1-1.py
@@ -1,20 +1,15 @@
 def is_prime(n):
-    prime_num_list = [2,3,5,7,9,11,13,17,19]
-    if n in prime_num_list:
-        print('%d is a prime nubmer' %n)
-    elif n == 1:
-        print('%d is not a prime number nor a synthetic number' %n)
+
+    if n<=0:
+        return print('%d is not a natural number.' %n)
+
+    if n == 1:
+        return print('%d is not a prime number nor a synthetic number' %n)
+
+
+    for i in range(2, n):
+        if n%i == 0:
+            return print('%d is a synthetic number' %n)
+    
     else:
-        print('%d is not a synthetic number' %n)
-
-def factorial(n):
-    if n == 0 or n == 1:
-        return 1
-    else:
-        return n*factorial(n-1)
-
-
-def find_gcd(a,b):
-    pass
-
-
+        return print('%d is a prime nubmer' %n)
