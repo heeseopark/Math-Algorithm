e70c709ba6bde42a761199014f5abb9befa907b2
heeseopark <heeseopark@korea.ac.kr>
2023-01-04 20:09:46 +0900

    0104 commit

---
+++ class expression.py
@@ -0,0 +1,11 @@
+class term():
+    def __init__(self,coefficient,order):
+        self.coefficient = coefficient
+        self.order = order
+    def get_coefficient():
+        co = self[0]
+        return co
+    def get_order():
+        
+
+class expression(term):
