5e8b190f7b5a0b2c4efc56ba2537bc19cc4bdf9c
heeseopark <heeseopark@korea.ac.kr>
2023-01-05 20:30:48 +0900

    0105 commit

--- class expression.py
+++ class expression.py
@@ -1,4 +1,6 @@
-class term():
+import re
+
+class term_class():
     def __init__(self,coefficient,order):
         self.coefficient = coefficient
         self.order = order
@@ -6,6 +8,22 @@ class term():
         co = self[0]
         return co
     def get_order():
-        
+    
 
-class expression(term):
+
+terms = expression.split(' ')
+print(terms)
+
+# ['3x', '+', '4y', '-', '2z']
+
+
+
+for term in terms:
+    match = re.search(r'([+-]?\d*)(\w+)', term)
+    coefficient = int(match.group(1)) if match.group(1) else 1
+    variable = match.group(2)
+    print(f'{coefficient} {variable}')
+
+# 3 x
+# 4 y
+#-2 z
